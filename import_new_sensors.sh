1eab14960cb194a5b7b58aa0a7d4122ca46355eb
marcel <admin@moerbst.de>
2017-01-03 15:22:21 +0100

    Update import_new_sensors.sh
    - fixed loop variable (line 26)
    - added "grep ." in loop to prevent creating empty sensor entry (line 26, see: http://stackoverflow.com/questions/1611809/remove-empty-lines-in-a-text-file-via-grep )

--- import_new_sensors.sh
+++ import_new_sensors.sh
@@ -23,6 +23,4 @@ echo "CREATE TABLE IF NOT EXISTS $MYSQLDB.$MYSQLTABLE ( timestamp BIGINT, id VAR
 while read LINE
 do
          echo $LINE | awk '{print "INSERT INTO '"$MYSQLDB"'.'"$MYSQLTABLE"' (timestamp, id, location, place, type, platform, ipv4, ipv6, label) VALUES ('"$TS"', \x27"$1"\x27, \x27"$2"\x27, \x27"$3"\x27, \x27"$4"\x27, \x27"$5"\x27, \x27"$6"\x27, \x27"$7"\x27, \x27"$8"\x27);" }' | mysql -u $MYSQLUSER -p$MYSQLPASS -h $MYSQLHOST -P $MYSQLPORT;
-done < <(cat templog_sensors.txt | egrep -v ^# | uniq)
-##done < <(egrep -v "^#" $SENSORSFILE | awk '{print $4}' | uniq)
-
+done < <(cat $SENSORFILE | egrep -v ^# | uniq | grep .)
